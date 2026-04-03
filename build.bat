e0df29c999efe0b236fa2b92bd724a35f82b7f8b
CarlosOlv <carlosolivi@yahoo.com>
2026-04-03 11:05:03 -0300

    Add Padding to 4MB on build.bat. Cartridge header have ASCII-X ID for openMSX

--- build.bat
+++ build.bat
@@ -3,12 +3,40 @@ cls
 
 tniasm.exe %1 %2
 
+
 rem Do Error Beep (BEL)
 set ERR=%ERRORLEVEL%
 if %ERR% neq 0 (
     @echo 
     @echo 
 )
-exit /b %ERR%
+
+del tniasm.tmp
+
+
+if %ERR% neq 0 (Exit /b %ERR%)
+
+rem Padding for 4Mb
+rem ---------------
+
+set TARGET=4194304
+set FILE=%2
+
+for %%F in (%FILE%) do set SIZE=%%~zF
+
+if %SIZE% GEQ %TARGET% (
+  echo Already %SIZE% bytes, do nothing
+  goto :EOF
+)
+
+set /a PAD=%TARGET%-%SIZE%
+rem echo Padding %FILE% de %SIZE% para %TARGET% bytes...
+
+fsutil file createnew pad.tmp %PAD% >nul
+@echo off
+copy /b %FILE% + pad.tmp %FILE%.tmp >nul 
+move /y %FILE%.tmp %FILE% >nul
+
+del pad.tmp
 
 
